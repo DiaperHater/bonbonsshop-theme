06f23278b36b0299f1d4af4c194dab4ec8fae3e5
Valery Pronin <pownmymail@gmail.com>
2024-03-13 21:57:19 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php get_header() ?>
+
+<?php
+
+$me = "valery";
+?>
+
+<section class="max-w-4xl px-12 mx-auto mt-24">
+  <h1 class="text-5xl font-semibold text-center text-red-500">
+    Bonbonsshoptheme is in da house!
+  </h1>
+  <img src="<?= get_template_directory_uri() ?>/assets/images/me.jpg" alt="Me" class="max-w-xl mx-auto mt-12 rounded-lg">
+</section>
+
+<?php get_footer() ?>
