b70db17de12d519e0615bec6e68307682b92885f
Valery Pronin <pownmymail@gmail.com>
2024-03-29 23:13:53 +0300

    feat: Minor adjustments

--- page.php
+++ page.php
@@ -2,16 +2,19 @@
 
 <section class="py-24 md:py-32">
   <div class="wrapper">
-    <?php
+    <div class="max-w-3xl mx-auto prose prose-lg prose-h2:text-new-york-pink">
 
-    if (have_posts()) {
-      while (have_posts()) {
-        the_post();
-        the_content();
+      <?php
+
+      if (have_posts()) {
+        while (have_posts()) {
+          the_post();
+          the_content();
+        }
       }
-    }
 
-    ?>
+      ?>
+    </div>
   </div>
 </section>
 
