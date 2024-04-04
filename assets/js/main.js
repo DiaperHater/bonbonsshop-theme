8863aac150fc58011c9f7fb9e86c94a1b9717253
Valery Pronin <pownmymail@gmail.com>
2024-04-04 06:38:07 +0300

    feat: Style single product variable select chevron

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,44 @@
     label.textContent = "Сюда можно загрузить фотографию с примером оформления";
   }
 }
+
+// {
+//   const observer = new MutationObserver(callback);
+//   const target = document.querySelector('.bonbonsshop');
+//   const params = {
+//     childList: true,
+//     subtree: true
+//   };
+
+//   observer.observe(target, params);
+
+//   function callback(records) {
+
+//     for (const record of records) {
+
+//       if (record.type !== 'childList') {
+
+//         continue;
+//       }
+
+//       for (const node of record.addedNodes) {
+
+//         if (node.nodeType !== Node.ELEMENT_NODE || node.nodeName === 'SCRIPT') {
+
+//           continue;
+//         }
+
+//         const targetElList = node.querySelectorAll('.wc-block-components-product-details__name');
+//         for (const el of targetElList) {
+
+//           if (el.innerText === 'Файл загружен:') {
+
+//             el.innerText = 'Fuck you all!';
+//           }
+//         }
+//       }
+
+
+//     }
+//   }
+// }
