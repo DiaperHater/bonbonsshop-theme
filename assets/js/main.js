326c2c2241f45b23acde63818d4ea865764301af
Valery Pronin <pownmymail@gmail.com>
2024-04-04 04:16:46 +0300

    feat: Change label text for single product page file upload component

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,9 @@
 "use strict";
 
-console.log('js is fine!');
+// Change label text for single product page file upload component
+{
+  const label = document.querySelector('.wau_wrapper_div label');
+  if (label) {
+    label.textContent = "Сюда можно загрузить фотографию с примером оформления";
+  }
+}
