c9c300ccf2ca016ff35de56bd5e86337d19725df
Valery Pronin <pownmymail@gmail.com>
2024-04-11 06:45:08 +0300

    feat: Update hero text

--- page-shop.php
+++ page-shop.php
@@ -8,10 +8,10 @@
       <!-- Column -->
       <div class="lg:col-span-8 xl:col-span-7">
         <!-- Heading -->
-        <h2 class="section-heading">
-          Подарочные наборы
-          <br>к 8 марта
-        </h2>
+        <h1 class="section-heading">
+          <span class="inline-block mb-2 text-4xl font-normal">Кондитерская</span>
+          <br>BONBONS
+        </h1>
         <!-- Image (mobile only) -->
         <figure class="mt-24 w-[280px] aspect-[5/7] relative mx-auto group lg:hidden">
           <span class="absolute inset-0 duration-200 border-2 border-new-york-pink/20 rounded-2xl -rotate-6 group-hover:rotate-0"></span>
@@ -19,13 +19,13 @@
         </figure>
         <!-- Subheading -->
         <p class="mt-12 text-2xl text-zinc-500 lg:mt-40">
-          Любимые десерты
-          <br>для любимых людей
+          Онлайн заказ и доставка десертов
+          <br>в Солигорске
         </p>
         <!-- GoDownBottun -->
         <a href="#products" class="grid w-24 mx-auto mt-12 font-bold tracking-wide text-center text-white rounded-full aspect-square bg-new-york-pink place-items-center sm:mx-0 lg:w-44 lg:text-2xl">
           ВЫБРАТЬ
-          <br>НАБОР
+          ДЕСЕРТ
         </a>
       </div>
       <!-- Column -->
